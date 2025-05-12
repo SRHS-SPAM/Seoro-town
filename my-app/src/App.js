f17a74081d63c5863a6f4dfc8fc70ddcdc52eeb7
junseok05030 <164356571+junseok05030@users.noreply.github.com>
2025-05-12 20:22:00 +0900

    Navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,47 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+
+import Board from './page/Boardpage';
+import Schedule from './page/Schedule';
+import Notice from './page/Notice';
+import Grade from './page/Grade';
+import Club from './page/Club';
+import Market from './page/Market';
+
 
 function App() {
   return (
-    <div>
-      {/* 상단 네비게이션 바 */}
-      <div className="NavBar">
-        <div className="NavLeft">
-          <img src="RobotLogo.png" alt="로고" className="RobotLogo" />
-          RobotTown
+    <Router>
+      <div>
+        <div className="NavBar">
+          <div className="NavLeft">
+            <img src="RobotLogo.png" alt="로고" className="RobotLogo" />
+            <span className="BrandName">ROBOTOWN</span>
+          </div>
+          
+          <div className="NavCenter">
+            <NavLink to="/Board" className={({isActive}) => isActive ? "NavItem active" : "NavItem"}>게시판</NavLink>
+            <NavLink to="/Schedule" className={({isActive}) => isActive ? "NavItem active" : "NavItem"}>시간표</NavLink>
+            <NavLink to="/Notice"className={({ isActive }) => isActive ? "NavItem active" : "NavItem"}>가정통신문</NavLink>
+            <NavLink to="/Grade" className={({isActive}) => isActive ? "NavItem active" : "NavItem"}>학점계산기</NavLink>
+            <NavLink to="/Club" className={({isActive}) => isActive ? "NavItem active" : "NavItem"}>동아리</NavLink>
+            <NavLink to="/Market" className={({isActive}) => isActive ? "NavItem active" : "NavItem"}>서로당근</NavLink>
+          </div>
+          <div className="NavRight">
+            <span>로그인</span>
+            <span>회원가입</span>
+          </div>
         </div>
-
-        <div className="NavCenter">
-          <span>게시판</span>
-          <span>시간표</span>
-          <span>가정통신문</span>
-          <span>학점계산기</span>
-          <span>동아리</span>
-        </div>
-
-        <div className="NavRight">
-          <span>로그인</span>
-          <span>회원가입</span>
-        </div>
+        <Routes>
+          <Route path="/Board" element={<Board />} />
+          <Route path="/Schedule" element={<Schedule />} />
+          <Route path="/Notice" element={<Notice />} />
+          <Route path="/Grade" element={<Grade />} />
+          <Route path="/Club" element={<Club />} />
+          <Route path="/arket" element={<Market />} />
+        </Routes>
       </div>
-
-
-      <div className="Title">
-        <img src="pngwing.com.png" alt="서로타운" className="Logo" />
-        서로타운에 오신 여러분들 환영합니다
-      </div>
-    </div>
+    </Router>
   );
 }
 
