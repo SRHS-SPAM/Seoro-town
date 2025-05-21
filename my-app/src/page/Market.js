7e5e341705d1e2198341dfecdbc97bb05525a7ba
jyqaz7548 <165270263+jyqaz7548@users.noreply.github.com>
2025-05-21 17:43:12 +0900

    타이틀 + 검색창

--- my-app/src/page/Market.js
+++ my-app/src/page/Market.js
@@ -1,5 +1,7 @@
 import './Boardpage.css'; // 페이지 명에 맞게 수정
 import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import { Search, PenLine, FileText, Clock, MessageSquare, ThumbsUp, AlertCircle } from 'lucide-react'; // Lucide 아이콘 추가
+
 
     function Boardpage() {
         return (
@@ -23,6 +25,20 @@ import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-do
                 <span>로그인</span>
                 <span>회원가입</span>
             </div>
+            </div>
+            <div className="Title">
+                <img src="pngwing.com.png" alt="어서오고" className="ping" />
+                학생들이 물건을 나누는 공간이에요!
+            </div>
+            <div className="SearchContainer">{/* 검색창 */}
+                <div className="SearchBox">
+                    <div className="SearchPrefix">중고거래</div>
+                    <div className="SearchDivider"></div>
+                    <input type="text" className="SearchInput" placeholder="검색어를 입력하세요!" />
+                    <button className="SearchButton">
+                        <Search size={22} />
+                    </button>
+                </div>
             </div>
         </div>
         )
