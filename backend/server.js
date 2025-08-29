642abf39ce40a36beb5d46242d7edbc32c9ba741
jyqaz7548 <jyqaz7548@gmail.com>
2025-08-29 20:33:10 +0900

    백엔드 수정

--- backend/server.js
+++ backend/server.js
@@ -87,8 +87,8 @@ const startServer = async () => {
         });
 
         const PORT = process.env.PORT || 3001;
-        server.listen(PORT, () => {
-            console.log(`Server is running on http://localhost:${PORT}`);
+        server.listen(PORT, '0.0.0.0', () => {
+            console.log(`Server is running on port ${PORT}`);
         });
 
     } catch (error) {
