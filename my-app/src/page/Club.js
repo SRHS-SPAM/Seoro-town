49f5d8cf15e2b056746f1225a374b36bb970674a
jyqaz7548 <jyqaz7548@gmail.com>
2025-09-04 21:09:10 +0900

    줄바꿈 수정

--- my-app/src/page/Club.js
+++ my-app/src/page/Club.js
@@ -7,7 +7,7 @@ function Club() {
 
     const clubs = [
         { id: 1, name: 'DoubleM', popupImage: '/DoubleM.jpg', description: '안녕하세요. 서울로봇고등학교 전공자율동아리 DoubleM입니다. \n  아두이노와 라즈베리파이등을 활용하여 다양한 로봇들을 제작하고 있습니다!\n Notion:https://horn-flame-f50.notion.site/DoubleM-1f047aeeaf0880f3b3d3e79027a79ac5' },
-        { id: 2, name: 'CreRobot', popupImage: '/CreRobot.jpg', description: '저희 크레로봇은 로보티즈 사의 다이나믹셀 및 여러가지 제어기를 활용하여  창작로봇 및 휴머로이드를 직접 제작해볼수 있는 동아리입니다.\n 이 외에도 자신이 만든 창작 로봇을 가지고 로보플러스 직업박람회등 다양한 대회도 참여하여 자신의 커리어를 쌓아 나갈수있습니다' },
+        { id: 2, name: 'CreRobot', popupImage: '/CreRobot.jpg', description: '저희 크레로봇은 로보티즈 사의 다이나믹셀 및 여러가지 제어기를 활용하여 \n 창작로봇 및 휴머로이드를 직접 제작해볼수 있는 동아리입니다.\n 이 외에도 자신이 만든 창작 로봇을 가지고 로보플러스 직업박람회등 \n다양한 대회도 참여하여 자신의 커리어를 쌓아 나갈수있습니다' },
         { id: 3, name: 'SPAM', popupImage: '/SPAM.jpg', description: '서울로봇고등학교의 유일한 플랫폼 개발 동아리 입니다.\n WEB, APP, UIUX DESIGN, SERVER, GAME 등의 다양한 분야를 공부하며 자신의 역량을 키울 수 있습니다. \n 또한 컨퍼런스와 각종 대회에 참가하며 많은 발표와 입상을 통해 폭 넓은 동아리활동을 할 수 있습니다. \n Github:https://github.com/SRHS-SPAM' },
         { id: 4, name: '신청하세요', popupImage: '/white.jpg', description: '' },
         { id: 5, name: '신청하세요', popupImage: '/white.jpg', description: '' },
